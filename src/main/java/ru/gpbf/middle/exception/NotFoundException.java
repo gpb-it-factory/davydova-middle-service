23aa05c4dc8c4ed460f1de8b1c6d002beb129a4a
alina-gasprom <alina.k.91@icloud.com>
2024-08-14 15:59:53 +0300

    baddd commit

--- src/main/java/ru/gpbf/middle/exception/NotFoundException.java
+++ src/main/java/ru/gpbf/middle/exception/NotFoundException.java
@@ -5,6 +5,6 @@ public class NotFoundException extends RuntimeException {
 
     public NotFoundException(String message) {
         super(message);
-        System.out.println("kk");
+        System.out.println("kal");
     }
 }
